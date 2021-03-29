e7cf07722a041e43b6423c418fd02d9ac74e9c52
bigface <904919863@qq.com>
2021-03-29 15:55:55 +0800

    测试多线程爬虫

--- multyThreadTest.py
+++ multyThreadTest.py
@@ -1,6 +1,7 @@
 import time
 import threading
 import requests
+from bs4 import BeautifulSoup
 
 urls = [
     f"http://www.cnblogs.com/#p{page}"
@@ -9,18 +10,14 @@ urls = [
 
 def craw(url):
     html = requests.get(url)
-    print(url,len(html.text))
+    return html.text
 
-def main():
-    print("single process start")
-    start = time.time()
+def single_process_spider(urls):
     for url in urls:
         craw(url)
-    end = time.time()
-    print("single process end, time = ", end-start," seconds")
+        
 
-    print("multy process start")
-    start = time.time()
+def multy_process_spider(urls):
     threads = []
     for url in urls:
         th = threading.Thread(target=craw,args=(url,)) 
@@ -31,6 +28,23 @@ def main():
 
     for thread in threads:
         thread.join()
+
+def parse(html):
+    #class="post-item-title"
+    soup  = BeautifulSoup(html,"html.parser")
+    links = soup.find_all("a", class_="post-item-title")
+    return [(link["href"], link.get_text()) for link in links]
+
+def main():
+    print("single process start")
+    start = time.time()
+    single_process_spider(urls)
+    end = time.time()
+    print("single process end, time = ", end-start," seconds")
+
+    print("multy process start")
+    start = time.time()
+    multy_process_spider(urls)
     end = time.time()
     print("multy process end, time = ", end-start," seconds")
 
