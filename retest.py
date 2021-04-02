f20844dff99bcf6de53a5c920d66de5b05f0a28f
bigface <904919863@qq.com>
2021-04-02 14:06:33 +0800

    -

--- retest.py
+++ retest.py
@@ -1,6 +1,7 @@
 import re
 
-s = ' "dst":"这是一个触发器","prefixWrap":0,'
-j = re.findall(r'"dst":"(.+?)", "prefixWrap"',s)
+s = '{"rownum":79,"storeName":"瞿溪新天地","addressDetail":"瞿溪街道河头村瞿溪新天地广场一层","pro":"Wi-Fi,店内参观","provinceName":"浙江省","cityName":"温州市"},{"rownum":80,"storeName":"瞿溪新天地","addressDetail":"瞿溪街道河头村瞿溪新天地广场一层","pro":"Wi-Fi,店内参观","provinceName":"浙江省","cityName":"温州市"}]}'
+j = re.findall(r'"storeName":"(.+)","addressDetail"',s)
     # print(Json)
 print(j[0])
+# '[\u4e00-\u9fa5]+'
