bb55fec2617290ca0b4b5ad8b00f48f334958a3f
bigface <904919863@qq.com>
2021-04-09 15:29:04 +0800

    翻译结果示例JSON

---
+++ 03.result_json.js
@@ -0,0 +1,54 @@
+{
+    "trans_result": {
+        "data": [{
+            "dst": "\u4f60\u4e0d\u80fd\u6d88\u9664\u8c0e\u8a00",
+            "prefixWrap": 0,
+            "result": [
+                [0, "\u4f60\u4e0d\u80fd\u6d88\u9664\u8c0e\u8a00", ["0|23"],
+                    [],
+                    ["0|23"],
+                    ["0|21"]
+                ]
+            ],
+            "src": "You cannot kill the lie"
+        }],
+        "from": "en",
+        "status": 0,
+        "to": "zh",
+        "type": 2,
+        "phonetic": [{
+            "src_str": "\u4f60",
+            "trg_str": "n\u01d0"
+        }, {
+            "src_str": "\u4e0d",
+            "trg_str": "b\u00f9"
+        }, {
+            "src_str": "\u80fd",
+            "trg_str": "n\u00e9ng"
+        }, {
+            "src_str": "\u6d88",
+            "trg_str": "xi\u0101o"
+        }, {
+            "src_str": "\u9664",
+            "trg_str": "ch\u00fa"
+        }, {
+            "src_str": "\u8c0e",
+            "trg_str": "hu\u01ceng"
+        }, {
+            "src_str": "\u8a00",
+            "trg_str": "y\u00e1n"
+        }],
+        "keywords": [{
+            "means": ["\u6740\u6b7b", "\u5f04\u6b7b", "\u5bfc\u81f4\u6b7b\u4ea1", "\u6bc1\u706d", "\u7834\u574f", "\u627c\u6740", "\u4f7f\u505c\u6b62", "\u4f7f\u75db\u82e6", "\u4f7f\u75bc\u75db", "\u4f7f\u53d7\u6298\u78e8", "\u6740\u6b7b\uff0c\u6355\u6740", "\u88ab\u6355\u6740\u7684\u52a8\u7269", "\u730e\u7269"],
+            "word": "kill"
+        }, {
+            "means": ["\u8eba", "\u5e73\u8eba", "\u5e73\u5367", "\u5e73\u653e", "\u5904\u4e8e\uff0c\u4fdd\u7559\uff0c\u4fdd\u6301", "\u8c0e\u8a00", "\u4f4d\u7f6e", "\u8bf4\u8c0e", "\u6492\u8c0e", "\u7f16\u9020\u8c0e\u8a00", "\u8c0e\u8bdd"],
+            "word": "lie"
+        }]
+    },
+    "liju_result": {
+        "double": "",
+        "single": ""
+    },
+    "logid": 2118734215
+}
