0a31135656703673d394e2537b9da95af9b406da
bigface <904919863@qq.com>
2021-04-20 10:42:59 +0800

    面向监狱编程（bushi

--- test_scrapy/test_scrapy/spiders/test_Spider.py
+++ test_scrapy/test_scrapy/spiders/test_Spider.py
@@ -3,8 +3,11 @@ import scrapy
 
 class TestSpiderSpider(scrapy.Spider):
     name = 'test_Spider'
-    allowed_domains = ['www.baidu.com']
+    #允许的域名：限定start_urls列表中哪些url可以进行请求发送
+    # allowed_domains = ['www.baidu.com']
+    #起始url列表,列表中的urlscrapy会自动进行请求的发送
     start_urls = ['http://www.baidu.com/']
 
+    #数据解析:respose表示请求成功后的响应对象
     def parse(self, response):
-        pass
+        print(response)
