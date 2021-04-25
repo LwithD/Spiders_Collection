4a3f5e081781b8e1517fc098459743584c67dc57
bigface <904919863@qq.com>
2021-04-25 18:49:08 +0800

    -

--- qiubaiPro/qiubaiPro/spiders/qiubai.py
+++ qiubaiPro/qiubaiPro/spiders/qiubai.py
@@ -10,12 +10,19 @@ class QiubaiSpider(scrapy.Spider):
     #数据解析操作
     def parse(self, response):
         div_list = response.xpath('//div[@class="col1 old-style-col1"]/div')
+        all_data = []
         for div in div_list:
             #extract()方法提取selector对象的数据
             author = div.xpath('./div[1]/a[2]/h2/text()').extract_first()
             content = div.xpath('./a[1]/div/span[1]//text()').extract()
             content = ''.join(content)
 
-            print(author,content)
-            
-            break
+            dic = {
+                'author' : author,
+                'content' : content
+            }
+
+            all_data.append(dic)
+
+            # print(author,content)
+        return all_data
