21fadea93fde5f46b08c195cffe0279c84f385fd
bigface <904919863@qq.com>
2021-04-25 19:16:56 +0800

    爬虫本体

--- qiubaiPro/qiubaiPro/spiders/qiubai.py
+++ qiubaiPro/qiubaiPro/spiders/qiubai.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from qiubaiPro.items import QiubaiproItem
 
 class QiubaiSpider(scrapy.Spider):
     name = 'qiubai'
@@ -7,7 +7,27 @@ class QiubaiSpider(scrapy.Spider):
     start_urls = ['https://www.qiushibaike.com/text/']
 
 
-    #数据解析操作
+    #终端指令存储
+    # def parse(self, response):
+    #     div_list = response.xpath('//div[@class="col1 old-style-col1"]/div')
+    #     all_data = []
+    #     for div in div_list:
+    #         #extract()方法提取selector对象的数据
+    #         author = div.xpath('./div[1]/a[2]/h2/text()').extract_first()
+    #         content = div.xpath('./a[1]/div/span[1]//text()').extract()
+    #         content = ''.join(content)
+
+    #         dic = {
+    #             'author' : author,
+    #             'content' : content
+    #         }
+
+    #         all_data.append(dic)
+
+    #         # print(author,content)
+    #     return all_data
+
+    #基于管道存储
     def parse(self, response):
         div_list = response.xpath('//div[@class="col1 old-style-col1"]/div')
         all_data = []
@@ -16,13 +36,9 @@ class QiubaiSpider(scrapy.Spider):
             author = div.xpath('./div[1]/a[2]/h2/text()').extract_first()
             content = div.xpath('./a[1]/div/span[1]//text()').extract()
             content = ''.join(content)
+            item = QiubaiproItem()
+            #item.Field()方法基本上继承了字典，所以用字典的访问方式
+            item['author'] = author
+            item['content'] = content
 
-            dic = {
-                'author' : author,
-                'content' : content
-            }
-
-            all_data.append(dic)
-
-            # print(author,content)
-        return all_data
+            yield item
