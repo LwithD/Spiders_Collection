653a05206059cdef621cdd67bde55987e96758ef
bigface <904919863@qq.com>
2021-04-26 14:03:17 +0800

    管道代码，接受item对象并存储

--- qiubaiPro/qiubaiPro/pipelines.py
+++ qiubaiPro/qiubaiPro/pipelines.py
@@ -9,5 +9,22 @@ from itemadapter import ItemAdapter
 
 
 class QiubaiproPipeline:
+    fp = None
+    #覆写父类的方法:该方法只在开始爬虫的时候被调用一次
+    def open_spider(self,spider):
+        print('开始爬虫......')
+        self.fp = open('./qiubai.txt','w',encoding='utf-8')
+
+    #用来接受和处理item类型对象
     def process_item(self, item, spider):
+        author = item['author']
+        content = item['content']
+        
+        self.fp.write(author+":"+content+"\n")
+
         return item
+
+    #爬虫结束时调用：只被调用一次
+    def close_spider(self,spider):
+        print('结束爬虫！')
+        self.fp.close()
