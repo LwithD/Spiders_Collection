ec33f4891b45c550bd626b8a2db9379bb7f0c354
bigface <904919863@qq.com>
2021-04-29 18:16:30 +0800

    创建img管道

--- imgsPro/imgsPro/pipelines.py
+++ imgsPro/imgsPro/pipelines.py
@@ -6,8 +6,26 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.pipelines.images import ImagesPipeline
+import scrapy
 
+# class ImgsproPipeline:
+#     def process_item(self, item, spider):
+#         return item
 
-class ImgsproPipeline:
-    def process_item(self, item, spider):
+class ImgsproPipeline(ImagesPipeline):
+
+    #根据图片地址进行图片数据的请求
+    def get_media_requests(self, item, info):
+        yield scrapy.Request(item['src'])
+        # return super().get_media_requests(item, info)
+
+    def file_path(self, request, response=None, info=None):
+        imgName = request.url.split('/')[-1]
+        return imgName
+        # return super().file_path(request, response=response, info=info, *, item=item)
+
+    def item_completed(self, results, item, info):
         return item
+        #返回给下一个管道类
+        # return super().item_completed(results, item, info)
