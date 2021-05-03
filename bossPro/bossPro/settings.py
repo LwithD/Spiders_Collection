a893dc1ad09a2b6f6fe7d15597c75857a85def41
bigface <904919863@qq.com>
2021-05-03 14:49:02 +0800

    开启下载中间件

--- bossPro/bossPro/settings.py
+++ bossPro/bossPro/settings.py
@@ -52,9 +52,9 @@ ROBOTSTXT_OBEY = False
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'bossPro.middlewares.BossproDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   'bossPro.middlewares.BossproDownloaderMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
