88a3225b9aa082c055799a4f700dc80adaf34aa0
bigface <904919863@qq.com>
2021-05-04 15:47:30 +0800

    管道接收item数据并存储

--- News163_Spider/News163_Spider/pipelines.py
+++ News163_Spider/News163_Spider/pipelines.py
@@ -9,5 +9,19 @@ from itemadapter import ItemAdapter
 
 
 class News163SpiderPipeline:
+    fp = None
+    def open_spider(self,spider):
+        print('开始爬虫......')
+        self.fp = open('./news163.txt','w',encoding='utf-8')
+
     def process_item(self, item, spider):
+        author = item['title']
+        content = item['content']
+        
+        self.fp.write(author+":"+content+"\n")
         return item
+
+    def close_spider(self,spider):
+        print('结束爬虫！')
+        self.fp.close()
+
