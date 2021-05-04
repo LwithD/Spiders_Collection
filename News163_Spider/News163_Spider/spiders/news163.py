6d2fc8564a2f12d998008b8281d14fb2479136ae
bigface <904919863@qq.com>
2021-05-04 14:01:09 +0800

    网易新闻爬虫

---
+++ News163_Spider/News163_Spider/spiders/news163.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class News163Spider(scrapy.Spider):
+    name = 'news163'
+    allowed_domains = ['news.163.com']
+    start_urls = ['http://news.163.com/']
+
+    def parse(self, response):
+        pass
