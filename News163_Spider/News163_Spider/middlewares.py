b83fc177786bb881cb21605807daa5864c133cf0
bigface <904919863@qq.com>
2021-05-04 15:47:09 +0800

    中间件接收响应使用selenium获得动态加载数据

--- News163_Spider/News163_Spider/middlewares.py
+++ News163_Spider/News163_Spider/middlewares.py
@@ -4,69 +4,15 @@
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
 from scrapy import signals
-
+from scrapy.http import HtmlResponse
 # useful for handling different item types with a single interface
 from itemadapter import is_item, ItemAdapter
-
-
-class News163SpiderSpiderMiddleware:
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the spider middleware does not modify the
-    # passed objects.
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_spider_input(self, response, spider):
-        # Called for each response that goes through the spider
-        # middleware and into the spider.
-
-        # Should return None or raise an exception.
-        return None
-
-    def process_spider_output(self, response, result, spider):
-        # Called with the results returned from the Spider, after
-        # it has processed the response.
-
-        # Must return an iterable of Request, or item objects.
-        for i in result:
-            yield i
-
-    def process_spider_exception(self, response, exception, spider):
-        # Called when a spider or process_spider_input() method
-        # (from other spider middleware) raises an exception.
-
-        # Should return either None or an iterable of Request or item objects.
-        pass
-
-    def process_start_requests(self, start_requests, spider):
-        # Called with the start requests of the spider, and works
-        # similarly to the process_spider_output() method, except
-        # that it doesn’t have a response associated.
-
-        # Must return only requests (not items).
-        for r in start_requests:
-            yield r
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
-
+from time import sleep
 
 class News163SpiderDownloaderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
 
     def process_request(self, request, spider):
         # Called for each request that goes through the downloader
@@ -80,14 +26,25 @@ class News163SpiderDownloaderMiddleware:
         #   installed downloader middleware will be called
         return None
 
+    #拦截五大板块相应对象，进行修改
     def process_response(self, request, response, spider):
-        # Called with the response returned from the downloader.
+        bro = spider.bro        #获取爬虫类中定义的浏览器对象
 
-        # Must either;
-        # - return a Response object
-        # - return a Request object
-        # - or raise IgnoreRequest
-        return response
+        #挑选出指定的响应对象进行篡改
+        #通过url指定request
+        #通过request指定response
+
+        if request.url in spider.models_urls:
+            #基于selenium获取动态加载的数据
+            bro.get(request.url)
+            sleep(5)
+            page_text = bro.page_source #包含动态加载的数据
+            # print(page_text)
+            #实例化新的响应对象（包含动态加载）
+            new_response = HtmlResponse(url = request.url,body=page_text,encoding='utf-8',request=request)
+            return new_response
+        else:
+            return response
 
     def process_exception(self, request, exception, spider):
         # Called when a download handler or a process_request()
@@ -98,6 +55,3 @@ class News163SpiderDownloaderMiddleware:
         # - return a Response object: stops process_exception() chain
         # - return a Request object: stops process_exception() chain
         pass
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
