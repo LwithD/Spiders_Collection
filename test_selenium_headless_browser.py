5f025de9df61fa1c5c0702f57d87868c7b10f2c4
bigface <904919863@qq.com>
2021-05-05 16:39:44 +0800

    新selenium无头和反防检测

--- test_selenium_headless_browser.py
+++ test_selenium_headless_browser.py
@@ -6,17 +6,20 @@ options = EdgeOptions()
 options.use_chromium = True
 options.add_argument("headless")
 options.add_argument("disable-gpu")
-#防止打印无用信息   enable-automation规避检测
+#防止打印无用信息   enable-automation规避检测 #最新版浏览器已无用
 options.add_experimental_option("excludeSwitches", ['enable-automation', 'enable-logging'])
 
 
-#谷歌无头
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# chrome_options = Options()
+#谷歌无头  #谷歌88.0版本可用
+# from selenium.webdriver import Chrome
+# from selenium.webdriver import ChromeOptions
+# options = ChromeOptions()
 # chrome_options.add_argument('--headless')
 # chrome_options.add_argument('--disable-gpu')
-# webdriver1 = webdriver.Chrome(executable_path='./chromedriver',chrome_options=chrome_options)
+# options.add_experimental_option("excludeSwitches", ["enable-automation",'enable-logging'])
+# options.add_argument("--disable-blink-features=AutomationControlled")
+# options.add_experimental_option('useAutomationExtension', False)
+# wd = Chrome(options=options)
 
 wd = Edge(options = options)
 wd.get('https://www.baidu.com')
